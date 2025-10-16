e9997002ded0336b91e8c0638b351e30f5ff248f
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-10-16 11:16:40 -0500

    feat: add highlighting for `<cmd>` rhs

---
+++ .changeset/polite-coats-end.md
@@ -0,0 +1,5 @@
+---
+"tree-sitter-vim-map-side": minor
+---
+
+Add highlighting for `<cmd>` rhs
--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,8 +1,17 @@
 (keycode) @character.special
 
-; avoids highlighting "<" within ERROR nodes. corner cases
+; avoids highlighting "<" within ERROR nodes (corner cases)
 (keycode
   [
     "<"
     ">"
   ] @punctuation.bracket)
+
+; highlighting for <cmd> and : rhs. avoids ERROR nodes (corner cases)
+(map_side
+  [
+    "<"
+    ">"
+  ] @punctuation.bracket)
+
+"\\|" @string.escape
